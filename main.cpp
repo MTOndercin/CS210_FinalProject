86d455b3b7150b5094f38c3f1f0ddb57ee1d93e6
MTOndercin <michal@ondercin.com>
2025-04-22 21:43:10 -0500

    Finished search and file loading
    finalized file loading and searching. Milestone 1 complete

--- main.cpp
+++ main.cpp
@@ -3,38 +3,31 @@
 //4/22/25
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
-class City
+struct City
 {
-public:
     string code;
     string name;
     double pop;
-
-    City(string c, string n, double p)
-    {
-        code = c;
-        name = n;
-        pop = p;
-    }
-    void print()
-    {
-        cout << name << ", " << code << " - " << pop << endl;
-    }
 };
+
+void print(City c)
+{
+    cout <<  c.name << ", " << c.code << " - " << c.pop << endl;
+}
 
 class Cache
 {
 public:
     City cache[10];
-    int next;
+    int next = 0;
 
-    Cache(): cache{}
-    {
-        next = 0;
-    }
+    Cache(): cache{} {};
 
     //add to cache
     void add(City c)
@@ -56,13 +49,10 @@ public:
     {
         for(int i = 0; i < 10; i++)
         {
-            if(cache[i].code == c)
+            if(cache[i].code == c && cache[i].name == n)
             {
-                if(n == cache[i].name)
-                {
-                    cache[i].print();
-                    return true;
-                }
+                print(cache[i]);
+                return true;
             }
         }
         return false;
@@ -72,9 +62,63 @@ public:
 int main()
 {
     //variables
-    Cache cache();
+    Cache cache;
+    string name = "", code = "", line = "", word = "";
 
+    //run loop
+    while (true)
+    {
+        //get input
+        cout << "Enter the name of the city (\"exit\" to exit): ";
+        getline(cin, name);
+        if (name == "exit")
+            break;
+        cout << "Enter the country code: ";
+        cin >> code;
+        cin.ignore();
 
+        //search function
+        bool found = false;
+        if (!cache.search(code, name))
+        {
+            //open/reopen file each time
+            ifstream file("world_cities.csv");
+            if (!file.is_open())
+            {
+                std::cerr << "Error opening file" << std::endl;
+                return 1;
+            }
 
+            //search through file
+            while (getline(file, line) && found == false)
+            {
+                stringstream ss(line);
+                vector<string> row;
+                while (getline(ss, word, ','))
+                {
+                    row.push_back(word);
+                }
+
+                //if found, add to cache and print
+                if (row[0] == code && row[1] == name)
+                {
+                    City city;
+                    city.code = row[0];
+                    city.name = row[1];
+                    city.pop = stod(row[2]);
+                    cache.add(city);
+                    print(city);
+                    found = true;
+                }
+            }
+        }
+        else
+        {
+            cout << "(Found in cache)" << endl;
+            found = true;
+        }
+        if (found == false)
+            cout << "City not found" << endl;
+    }
     return 0;
 }
